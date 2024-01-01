2574fc87f64b230b75515290ceecb9f5db492106
ramesh banakar <ramesh.s.b@campusuvce.in>
2024-01-01 17:02:18 +0530

    default router for the Error handling

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ connectDb();
 app.use(express.json());
 app.use(express.static("./public"))
 app.use("/api/movies", moviesRouter);
+app.all("*",(req,res,next)=>{
+    res.status(404).send("404 Error page not found")
+})
 app.listen(8000, () => console.log("server has started@8000"));
